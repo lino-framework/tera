07d89f32f5925dd353b686fd946889470d2f383b
Luc Saffre <luc.saffre@gmail.com>
2017-06-29 23:44:28 +0300

    http://luc.lino-framework.org/blog/2017/0629.html

--- lino_tera/lib/tera/settings.py
+++ lino_tera/lib/tera/settings.py
@@ -19,6 +19,8 @@ class Site(Site):
     version = SETUP_INFO['version']
     url = "http://tera.lino-framework.org/"
 
+    demo_fixtures = 'std demo minimal_ledger euvatrates demo2'.split()
+
     demo_fixtures = ['std', 'demo', 'demo2']
                      # 'linotickets',
                      # 'tractickets', 'luc']
@@ -64,6 +66,7 @@ class Site(Site):
         yield 'lino_xl.lib.sepa'
         yield 'lino_xl.lib.finan'
         yield 'lino_xl.lib.invoicing'
+        yield 'lino_xl.lib.declarations'
         # 'lino_xl.lib.projects',
         yield 'lino_xl.lib.blogs'
         yield 'lino_xl.lib.notes'
