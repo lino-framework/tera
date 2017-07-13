cbdb705e7761f8dfc9b17a1bc70d6b50c8eab12a
Luc Saffre <luc.saffre@gmail.com>
2017-07-13 15:19:11 +0300

    http://luc.lino-framework.org/blog/2017/0713.html

--- lino_tera/lib/tera/layouts.py
+++ lino_tera/lib/tera/layouts.py
@@ -13,10 +13,21 @@ ref name purchases_allowed group *"
 
 rt.models.accounts.Accounts.detail_layout = """
 ref:10 name
-group type id default_amount:10
+group type id default_amount:10 vat_column
 needs_partner clearable purchases_allowed  needs_ana ana_account
 ledger.MovementsByAccount
 """
+
+
+rt.models.system.SiteConfigs.detail_layout = """
+site_company next_partner_id:10
+default_build_method simulate_today
+clients_account   sales_account
+suppliers_account purchases_account tax_offices_account
+site_calendar default_event_type #pupil_guestrole
+max_auto_events hide_events_before
+"""
+
 
 # rt.models.vat.ItemsByInvoice.column_names = "account title ana_account vat_class total_base total_vat total_incl"
 
