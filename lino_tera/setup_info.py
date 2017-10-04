3c01b9b4ce04a70c643df51495a5c1535a451b11
Luc Saffre <luc.saffre@gmail.com>
2017-10-05 02:00:15 +0300

    http://luc.lino-framework.org/blog/2017/1005.html

--- lino_tera/setup_info.py
+++ lino_tera/setup_info.py
@@ -6,7 +6,7 @@
 
 SETUP_INFO = dict(
     name='lino-tera',
-    version='17.9.0',
+    version='17.10.0',
     install_requires=['lino-xl'],
     # tests_require=['pytest', 'mock'],
     test_suite='tests',
