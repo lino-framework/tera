671db709cf65579a467e53360e33deca72a67556
Luc Saffre <luc.saffre@gmail.com>
2018-08-05 20:18:33 +0300

    http://luc.lino-framework.org/blog/2018/0805.html

--- README.rst
+++ README.rst
@@ -31,5 +31,9 @@ Lino Tera is a customizable management system for therapeutic centres.
   please see `www.saffre-rumma.net
   <http://www.saffre-rumma.net/noi/>`__.
 
+- There is also an official --and uncomplete-- `User's Manual in
+  German
+  <https://github.com/lino-framework/tera/raw/master/docs/dl/Handbuch_Lino_Tera.pdf>`__.
+
 
 
--- lino_tera/setup_info.py
+++ lino_tera/setup_info.py
@@ -39,6 +39,10 @@ Lino Tera is a customizable management system for therapeutic centres.
   please see `www.saffre-rumma.net
   <http://www.saffre-rumma.net/noi/>`__.
 
+- There is also an official --and uncomplete-- `User's Manual in
+  German
+  <https://github.com/lino-framework/tera/raw/master/docs/dl/Handbuch_Lino_Tera.pdf>`__.
+
 
 """,
     author='Luc Saffre',
