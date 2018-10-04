935fe3850ba6daf3b80d55ba60a71992df4780a6
Luc Saffre <luc.saffre@gmail.com>
2018-10-04 21:23:33 +0300

    http://luc.lino-framework.org/blog/2018/1004.html

--- lino_tera/lib/teams/fixtures/demo.py
+++ lino_tera/lib/teams/fixtures/demo.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2016-2018 Luc Saffre
+# Copyright 2016-2018 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 """The `demo` fixture for this plugin."""
 
@@ -9,5 +9,5 @@ from lino.api import rt, _
 def objects():
     Team = rt.models.teams.Team
     yield Team(name="Eupen", ref="E")
-    yield Team(name="St.Vith", ref="S")
+    yield Team(name="St. Vith", ref="S")
 
