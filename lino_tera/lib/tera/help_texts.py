f761556ca73f2aa8a78f173cd569d7cf01c7e8be
Luc Saffre <luc.saffre@gmail.com>
2018-11-20 05:24:55 +0200

    http://luc.lino-framework.org/blog/2018/1119.html

--- lino_tera/lib/tera/help_texts.py
+++ lino_tera/lib/tera/help_texts.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 
 help_texts = {
-    'lino_tera.lib.tera.migrate.Migrator' : _("""The standard migrator for noi."""),
-    'lino_tera.lib.tera.migrate.Migrator.migrate_from_1_0_1' : _("""Move Deployment and Milestone from 'tickets' to new plugin
-'deploy'."""),
+    'lino_tera.lib.tera.migrate.Migrator' : _("""The standard migrator for tera."""),
+    'lino_tera.lib.tera.migrate.Migrator.migrate_from_18_8_0' : _("""min_asset and number_of_events now in Tarif instead Product"""),
 }
