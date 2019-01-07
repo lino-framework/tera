59f53947f5868827082f7567c06c8e736a633c57
Luc Saffre <luc.saffre@gmail.com>
2019-01-07 14:59:37 +0200

    http://luc.lino-framework.org/blog/2019/0107.html

--- lino_tera/lib/tera/migrate.py
+++ lino_tera/lib/tera/migrate.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2016-2018 Rumma & Ko Ltd
+# Copyright 2016-2019 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 
 """
@@ -54,3 +54,120 @@ class Migrator(Migrator):
 
         return '18.11.0'
 
+    def migrate_from_18_12_0(self, globals_dict):
+
+        bv2kw = globals_dict['bv2kw']
+        courses_PriceRule = rt.models.courses.PriceRule
+        courses_Course = rt.models.courses.Course
+        courses_Enrolment = rt.models.courses.Enrolment
+
+        @override(globals_dict)
+        def create_courses_enrolment(id, start_date, end_date, printed_by_id,
+                                     user_id, course_area, course_id, pupil_id,
+                                     request_date, state, places, option_id,
+                                     remark, confirmation_details, tariff_id,
+                                     guest_role_id):
+            #    if course_area: course_area = settings.SITE.models.courses.CourseAreas.get_by_value(course_area)
+            #    if state: state = settings.SITE.models.courses.EnrolmentStates.get_by_value(state)
+            kw = dict()
+            kw.update(id=id)
+            kw.update(start_date=start_date)
+            kw.update(end_date=end_date)
+            kw.update(printed_by_id=printed_by_id)
+            kw.update(user_id=user_id)
+            kw.update(course_area=course_area)
+            kw.update(course_id=course_id)
+            kw.update(pupil_id=pupil_id)
+            kw.update(request_date=request_date)
+            kw.update(state=state)
+            kw.update(places=places)
+            kw.update(option_id=option_id)
+            kw.update(remark=remark)
+            kw.update(confirmation_details=confirmation_details)
+            # kw.update(tariff_id=tariff_id)
+            kw.update(guest_role_id=guest_role_id)
+            return courses_Enrolment(**kw)
+
+        @override(globals_dict)
+        def create_courses_course(id, modified, ref, start_date, start_time,
+                                  end_date, end_time, healthcare_plan_id,
+                                  user_id, every_unit, every, monday, tuesday,
+                                  wednesday, thursday, friday, saturday,
+                                  sunday, max_events, room_id, max_date,
+                                  line_id, teacher_id, slot_id, description,
+                                  remark, state, max_places, name,
+                                  enrolments_until, tariff_id, payment_term_id,
+                                  procurer_id, mandatory, ending_reason,
+                                  partner_tariff, translator_type,
+                                  therapy_domain, team_id, partner_id,
+                                  paper_type_id):
+            #    if every_unit: every_unit = settings.SITE.models.cal.Recurrencies.get_by_value(every_unit)
+            #    if state: state = settings.SITE.models.courses.CourseStates.get_by_value(state)
+            #    if ending_reason: ending_reason = settings.SITE.models.courses.EndingReasons.get_by_value(ending_reason)
+            #    if partner_tariff: partner_tariff = settings.SITE.models.courses.PartnerTariffs.get_by_value(partner_tariff)
+            #    if translator_type: translator_type = settings.SITE.models.courses.TranslatorTypes.get_by_value(translator_type)
+            #    if therapy_domain: therapy_domain = settings.SITE.models.courses.TherapyDomains.get_by_value(therapy_domain)
+            kw = dict()
+            kw.update(id=id)
+            kw.update(modified=modified)
+            kw.update(ref=ref)
+            kw.update(start_date=start_date)
+            kw.update(start_time=start_time)
+            kw.update(end_date=end_date)
+            kw.update(end_time=end_time)
+            kw.update(healthcare_plan_id=healthcare_plan_id)
+            kw.update(user_id=user_id)
+            kw.update(every_unit=every_unit)
+            kw.update(every=every)
+            kw.update(monday=monday)
+            kw.update(tuesday=tuesday)
+            kw.update(wednesday=wednesday)
+            kw.update(thursday=thursday)
+            kw.update(friday=friday)
+            kw.update(saturday=saturday)
+            kw.update(sunday=sunday)
+            kw.update(max_events=max_events)
+            kw.update(room_id=room_id)
+            kw.update(max_date=max_date)
+            kw.update(line_id=line_id)
+            kw.update(teacher_id=teacher_id)
+            kw.update(slot_id=slot_id)
+            if description is not None: kw.update(
+                bv2kw('description', description))
+            kw.update(remark=remark)
+            kw.update(state=state)
+            kw.update(max_places=max_places)
+            kw.update(name=name)
+            kw.update(enrolments_until=enrolments_until)
+            # kw.update(tariff_id=tariff_id)
+            kw.update(payment_term_id=payment_term_id)
+            kw.update(procurer_id=procurer_id)
+            kw.update(mandatory=mandatory)
+            kw.update(ending_reason=ending_reason)
+            kw.update(partner_tariff=partner_tariff)
+            kw.update(translator_type=translator_type)
+            kw.update(therapy_domain=therapy_domain)
+            kw.update(team_id=team_id)
+            kw.update(partner_id=partner_id)
+            kw.update(paper_type_id=paper_type_id)
+            return courses_Course(**kw)
+
+        @override(globals_dict)
+        def create_courses_pricerule(id, seqno, fee_id, tariff, event_type_id,
+                                     pf_residence, pf_composition, pf_income):
+            #    if tariff: tariff = settings.SITE.models.courses.PartnerTariffs.get_by_value(tariff)
+            #    if pf_residence: pf_residence = settings.SITE.models.courses.Residences.get_by_value(pf_residence)
+            #    if pf_composition: pf_composition = settings.SITE.models.courses.HouseholdCompositions.get_by_value(pf_composition)
+            #    if pf_income: pf_income = settings.SITE.models.courses.IncomeCategories.get_by_value(pf_income)
+            kw = dict()
+            kw.update(id=id)
+            kw.update(seqno=seqno)
+            kw.update(fee_id=fee_id)
+            # kw.update(tariff=tariff)
+            kw.update(event_type_id=event_type_id)
+            kw.update(pf_residence=pf_residence)
+            kw.update(pf_composition=pf_composition)
+            kw.update(pf_income=pf_income)
+            return courses_PriceRule(**kw)
+
+        return '19.1.0'
