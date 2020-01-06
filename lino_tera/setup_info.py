f34d5c0efd29d9b7feeda8168dd40e28051155aa
Hamza Khchine <hamzakhchine@gmail.com>
2020-01-06 15:10:03 +0100

    Prep release v 20.1

--- lino_tera/setup_info.py
+++ lino_tera/setup_info.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2017-2018 Rumma & Ko Ltd
+# Copyright 2017-2020 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 
 # $ python setup.py test -s tests.PackagesTests.test_packages
