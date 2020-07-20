e8f99a5340f7d49e56d23d6c8305b932ad31b36a
Luc Saffre <luc.saffre@gmail.com>
2020-07-20 20:43:24 +0300

    release to pypi

--- lino_tera/setup_info.py
+++ lino_tera/setup_info.py
@@ -6,7 +6,7 @@
 
 SETUP_INFO = dict(
     name='lino-tera',
-    version='20.7.0',
+    version='20.7.1',
     install_requires=['lino-xl'],
     # tests_require=['pytest', 'mock'],
     test_suite='tests',
